bb189f8fd042be6a6882acc4e941f798d409fdf6
remi-1201 <remi.nkjm@gmail.com>
2022-03-14 11:40:24 +0900

    fixed 1

--- sample.js
+++ sample.js
@@ -3,21 +3,31 @@ new Vue({
   el: '#app',
   // dataは、定義されたVue.jsのインスタンスが持つ属性（値）
   data: {
-    name: 'A山B郎',
-    course: 'Webエンジニアコース',
-    acceptancePeriod: '2019年01月期',
-    defaultLastId: 3,
-    students: [
-      { id: 1, name: '野呂浩良', course: '機械学習コース', acceptancePeriod: '2019年01月期' },
-      { id: 2, name: '富永修司', course: 'Webエンジニアコース', acceptancePeriod: '2017年11月期' },
-      { id: 3, name: '斉藤一起', course: 'Webエンジニアコース', acceptancePeriod: '2017年11月期' }
-    ]
+    addName: '',
+    addCourse: '',
+    addPeriod: '',
+    list: [],
   },
   // methodsに、Vue.jsのインスタンスに使用させたいメソッドを記載する
   methods: {
-    addStudent: function() {
+    addToDo() {
         // この function() { } の中にaddStudentメソッド
         // （入力欄に入力された値と、ID値（最後に登録されたID値+1の値）が新たに出現する機能）の処理を書く
+        if (!this.addName.length) {
+          return
+        }else if(!this.addCourse.length) {
+          return
+        }else if(!this.addPeriod.length) {
+          return
+        }
+        this.list.push({
+          name: this.addName,
+          course: this.addCourse,
+          period: this.addPeriod
+        });
+        this.addName = '',
+        this.addCourse = '',
+        this.addPeriod = ''
     }
   }
 })
